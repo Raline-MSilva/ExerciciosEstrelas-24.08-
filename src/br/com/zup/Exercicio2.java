961e4a2d935a487db0542802c67daa5454170318
Raline Silva <ralinesilva1@gmail.com>
2021-08-24 23:31:01 -0300

    Add resolução ex.2

--- src/br/com/zup/Exercicio2.java
+++ src/br/com/zup/Exercicio2.java
@@ -1,2 +1,19 @@
-package br.com.zup;public class Exercicio2 {
+package br.com.zup;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        // criando as variaveis das notas
+        int nota1 = 7;
+        int nota2 = 5;
+        int nota3 = 6;
+        int nota4 = 8;
+
+        // criando a soma das notas
+        float somaDasNotas = nota1 + nota2 + nota3 + nota4;
+
+        //dividindo as notas e exibindo - as
+        float mediaDasNotas = somaDasNotas / 4;
+        System.out.println("Sua média é " + mediaDasNotas);
+
+    }
 }
