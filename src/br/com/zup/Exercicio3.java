405c876d1d44e0900365cbb739e514ce18d146c1
Raline Silva <ralinesilva1@gmail.com>
2021-08-25 16:43:59 -0300

    corrigindo erros

--- src/br/com/zup/Exercicio3.java
+++ src/br/com/zup/Exercicio3.java
@@ -6,14 +6,12 @@ public class Exercicio3 {
     public static void main (String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Por favor digite os metros ");
+        System.out.println("Por favor digite os metros para converter em centimetros ");
         float metros = leitor.nextFloat();
-        System.out.println("Por favor digite os centimetros");
-        float centimetros = leitor.nextFloat();
 
-        //calculando
+        //convertendo metros em centimetros
         float conversao = metros * 100;
         float resultado = conversao;
-        System.out.println("Sua conversão deu " + resultado);
+        System.out.println("Sua Conversão de metros para centimetro é " + resultado);
     }
 }
