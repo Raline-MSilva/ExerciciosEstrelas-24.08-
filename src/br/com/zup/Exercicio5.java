e2a2b475c097b6225657edfe56bed31a72a340bc
Raline Silva <ralinesilva1@gmail.com>
2021-08-25 22:53:08 -0300

    alterações

--- src/br/com/zup/Exercicio5.java
+++ src/br/com/zup/Exercicio5.java
@@ -1,13 +1,18 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Exercicio5 {
     public static void main (String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Por favor digite o valor farenheint para ser convertido em Celsius ");
+        double calculo = leitor.nextDouble();
 
         //convertendo farenheint em celsius
-        int f = 50;
-        int convertendoFemC = (f - 32) * 5/9;
+        double converteFemC = calculo - 32 * 5/9;
 
         //exibindo
-        System.out.println(convertendoFemC + "°");
+        System.out.println(converteFemC + "°");
     }
 }
